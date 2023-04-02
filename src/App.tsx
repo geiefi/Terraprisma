317aa2dbb7080eb78c0cb73cebeb3715e5332bbf
gabriel miranda <gabrielmfern@outlook.com>
2023-04-02 16:21:13 -0300

    feature: use proper identifications for the components such as the Steps component and the Form in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import { Component, createSignal } from 'solid-js';
+import { createStore } from 'solid-js/store';
+
+import { Form } from './Components/Form/Form';
 import Box from './Components/General/Box/Box';
 import Button from './Components/General/Button/Button';
 import Stack from './Components/Layout/Stack/Stack';
-
 import Steps, { Step } from './Components/Navigation/Steps/Steps';
+
+import { FormStore } from './Components/Form/FormContext';
 
 const App: Component = () => {
   const [currentStep, setCurrentStep] = createSignal<number>(1);
 
+  const formStore = createStore<FormStore<{}>>(new FormStore({}));
+
   return (<>
-    <Steps current={currentStep} identification='Testing steps'>
-      <Step>First</Step>
-      <Step>Second</Step>
-      <Step>Last</Step>
+    <Steps current={currentStep} identification='Passo à passo de compra'>
+      <Step>endereço</Step>
+      <Step>pagamento</Step>
+      <Step>conclusão</Step>
     </Steps>
 
     <Box>
-      <Stack spacing={5} direction='horizontal' align='space-between'>
-        <Button
-          size='large'
-          onClick={() => setCurrentStep(currentStep() - 1)}
-        >Previous</Button>
-        <Button
-          size='large'
-          onClick={() => setCurrentStep(currentStep() + 1)}
-        >Next</Button>
-      </Stack>
+      <Form formStore={formStore} indentification='Informações de pagamento'>
+        <Stack direction='horizontal' align='space-between'>
+          <Button
+            size='large'
+            onClick={() => setCurrentStep(currentStep() - 1)}
+          >Previous</Button>
+          <Button
+            size='large'
+            onClick={() => setCurrentStep(currentStep() + 1)}
+          >Next</Button>
+        </Stack>
+      </Form>
     </Box>
   </>);
 };
