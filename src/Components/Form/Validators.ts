7502d074342d9f79356f6fdded8f77bfaf099392
gabriel miranda <gabrielmfern@outlook.com>
2023-04-15 14:21:23 -0300

    feature: add a email Validator based on a regex test

--- src/Components/Form/Validators.ts
+++ src/Components/Form/Validators.ts
@@ -1,12 +1,28 @@
 import { FieldValidator, FieldValue } from "./FormContext";
 
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
 const Validators: Record<any, FieldValidator> = {
-  required: (value: any) => (typeof value === 'undefined')
+  /**
+    * Validates weather or not the field is undefined, null, empty when trimmed 
+    * or if being an array is also empty.
+    */
+  required: (value: FieldValue) => (typeof value === 'undefined')
     || (value === null)
     || (typeof value === 'string' && value.trim().length === 0)
     || (Array.isArray(value) && value.length === 0)
     ? 'This is a required field!'
     : undefined,
+  /**
+    * Validates weather or not the field matches a Regex email.
+    * 
+    * Regex: `/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g`
+    */
+  email: (value: FieldValue) => (typeof value === 'undefined')
+    || (value === null)
+    || emailRegex.test(value.toString().trim())
+    ? 'This is not a valid email!'
+    : undefined
 }
 
 export default Validators;
