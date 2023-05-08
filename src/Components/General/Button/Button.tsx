e396336aeee67e838f003227c30d0d7cb32d8999
gabriel miranda <gabrielmfern@outlook.com>
2023-05-07 21:57:20 -0300

    chore: improve empty button's background coloring

--- src/Components/General/Button/Button.tsx
+++ src/Components/General/Button/Button.tsx
@@ -15,36 +15,21 @@ export type ButtonProps = {
 
 const Button: Component<ButtonProps> = (props) => {
   const depth = useDepth() || (() => 0);
-  const backgroundColor = createMemo(() => {
-    if (props.type === 'empty') {
-      switch (depth()) {
-        case 0:
-          return 'var(--gray-1)';
-        case 1:
-          return 'var(--gray-0)';
-        case 2:
-          return 'var(--gray-1)';
-        case 3:
-          return 'var(--gray-2)';
-        case 4:
-          return 'var(--gray-3)';
-      }
-    } else if (props.type === 'default' || typeof props.type === 'undefined') {
-      return 'var(--primary-accent)';
-    }
-  });
 
   return <Ripple onClick={props.onClick} style={{ display: 'inline-block' }}>
     <button 
       class={props.class} 
+      type='button'
       classList={{
         'empty': props.type === 'empty',
+        'gray-0': depth() === 1,
+        'gray-1': depth() === 0 || depth() === 2,
+        'gray-2': depth() === 3,
+        'gray-3': depth() === 4,
+
         'small': props.size === 'small',
         'medium': props.size === 'medium' || typeof props.size === 'undefined',
         'large': props.size === 'large'
-      }}
-      style={{
-        '--bg-color': backgroundColor()
       }}
     >{props.children}</button>
   </Ripple>;
