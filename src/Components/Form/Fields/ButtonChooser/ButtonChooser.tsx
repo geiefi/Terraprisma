23720fb2392e93cd100864a1631110c099223ab8
gabriel miranda <gabrielmfern@outlook.com>
2023-05-09 20:19:43 -0300

    feature: add a <ButtonChooser> component for button choices

---
+++ src/Components/Form/Fields/ButtonChooser/ButtonChooser.tsx
@@ -0,0 +1,95 @@
+import { Component, createMemo, children as accessChildren, JSX, on, ParentProps, createEffect, For } from "solid-js";
+import Button from "../../../General/Button/Button";
+import { FieldValue } from "../../FormContext";
+
+import FormControl from "../_Shared/FormControl/FormControl";
+import { FieldProps, setupCommunicationWithFormContext, setupFieldsValueSignal } from "../_Shared/Utilts";
+
+import "./ButtonChooser.scss";
+
+export interface ButtonChooserProps extends FieldProps, ParentProps {
+  label: JSX.Element;
+  helperText?: JSX.Element;
+
+  onChange?: (newValue: FieldValue) => any;
+}
+
+export interface OptionProps extends ParentProps {
+  value: string;
+}
+
+const Option: Component<OptionProps> = (props) => {
+  return props as unknown as JSX.Element;
+};
+
+const ButtonChooser = (props: ButtonChooserProps) => {
+  const form = setupCommunicationWithFormContext(props);
+  const [value, setValue] = setupFieldsValueSignal(props, form);
+
+  const id = createMemo(() =>
+    form
+      ? `button-chooser-${form.identification()}-${props.name}`
+      : `button-chooser-${props.name}`
+  );
+  const hasContent = createMemo(() =>
+    (value() || '').toString().length > 0
+  );
+
+  const getChildren = accessChildren(() => props.children);
+  const options = createMemo<OptionProps[]>(() => {
+    let childrenArr: (JSX.Element | OptionProps)[];
+
+    const children = getChildren();
+    if (Array.isArray(children)) {
+      childrenArr = children;
+    } else {
+      childrenArr = [children];
+    }
+
+    return childrenArr.filter(child => {
+      return child !== null
+        && typeof child === 'object'
+        && Object.hasOwn(child, 'value')
+        && Object.hasOwn(child, 'children');
+    }) as OptionProps[];
+  });
+
+  createEffect(on(value, () => {
+    if (form) {
+      form.validate(props.name);
+    }
+  }));
+
+  return <div
+    class="button-chooser"
+    id={id()}
+    classList={{
+      'error': form?.hasErrors(props.name)
+    }}
+  >
+    <span class="label">{props.label}</span>
+
+    <div class="buttons">
+      <For each={options()}>{(opt) => (
+        <Button
+          type="empty"
+          classList={{
+            'active': opt.value === value()
+          }}
+          onClick={() => setValue(opt.value)}
+        >{opt.children}</Button>
+      )}</For>
+    </div>
+
+    {(props.helperText || form?.hasErrors(props.name))
+      && <div class="helper-text">{
+        form?.hasErrors(props.name)
+          ? form.firstErrorFor(props.name)
+          : props.helperText
+      }</div>}
+  </div>;
+};
+
+ButtonChooser.Option = Option;
+
+export default ButtonChooser;
