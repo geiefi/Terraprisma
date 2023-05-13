cb3cd08744dd5b85c517a00ad8124997d0d7d4bc
gabriel miranda <gabrielmfern@outlook.com>
2023-05-13 10:10:50 -0300

    feature: improve the hover effect of the InputContainer and add some more props to it

--- src/Components/Form/Fields/_Shared/InputContainer/InputContainer.tsx
+++ src/Components/Form/Fields/_Shared/InputContainer/InputContainer.tsx
@@ -1,25 +1,39 @@
 import type { Accessor, Component, JSX, ParentProps } from "solid-js";
 
+import { useDepth } from "../../../../General/Box/Box";
+
 import './InputContainer.scss';
 
 export type InputContainerProps = ParentProps<{
   id: Accessor<string>;
   label?: JSX.Element;
+  class?: string,
+  classList?: Record<string, boolean | undefined>,
+  style?: JSX.CSSProperties,
+
+  ref?: (inputContainer: HTMLDivElement) => any,
+
   focused: Accessor<boolean>,
   hasContent: Accessor<boolean>,
-  style?: JSX.CSSProperties,
-  ref?: (inputContainer: HTMLDivElement) => any,
   onClick?: (event: MouseEvent) => any,
 }>;
 
 const InputContainer: Component<InputContainerProps> = (props) => {
+  const depth = useDepth() || (() => 0);
+
   return <div 
-    class='input-container'
+    class={props.class 
+      ? props.class + ' input-container'
+      : 'input-container'}
     ref={props.ref}
     style={props.style}
     classList={{
       focused: props.focused(),
-      hasContent: props.hasContent()
+      hasContent: props.hasContent(),
+
+      'inside-box': depth() > 0,
+
+      ...props.classList
     }}
     onClick={props.onClick}
   >
