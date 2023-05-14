caecbfddd3a9e7a5b0c027f48e218c28be3cc833
gabriel miranda <gabrielmfern@outlook.com>
2023-05-14 16:20:20 -0300

    feature: improve Button's code and add 'icon' and 'rounded' button types

--- packages/foxpox/src/Components/General/Button/Button.tsx
+++ packages/foxpox/src/Components/General/Button/Button.tsx
@@ -7,7 +7,7 @@ import './Button.scss';
 export type ButtonProps = ParentProps<{
   color?: 'primary' | 'secondary' | 'tertiary',
   size?: 'small' | 'medium' | 'large',
-  type?: 'default' | 'empty',
+  type?: 'default' | 'empty' | 'icon' | 'rounded',
 
   disabled?: boolean,
 
@@ -23,57 +23,46 @@ const Button: Component<ButtonProps> = (props) => {
 
   const color = createMemo(() => props.color || 'primary');
 
-  return (<>
-    {props.disabled
-      ? <button
-        class={props.class}
-        type='button'
-        style={props.style}
-        classList={{
-          'disabled': props.disabled,
+  return <Ripple
+    onClick={(event) => {
+      if (props.onClick && !props.disabled) {
+        props.onClick(event);
+      }
+    }}
+    noRipple={props.disabled}
+    color={props.type === 'empty'
+      ? `var(--lightened-${color()})`
+      : undefined}
+    style={{ display: 'inline-block' }}
+  >
+    <button
+      class={props.class}
+      type='button'
+      style={props.style}
+      classList={{
+        'primary': color() === 'primary',
+        'secondary': color() === 'secondary',
+        'tertiary': color() === 'tertiary',
 
-          'empty': props.type === 'empty',
+        'disabled': props.disabled,
 
-          'small': props.size === 'small',
-          'medium': props.size === 'medium' || typeof props.size === 'undefined',
-          'large': props.size === 'large',
+        'icon': props.type === 'icon',
+        'rounded': props.type === 'rounded',
 
-          ...props.classList
-        }}
-      >{props.children}</button>
-      : <Ripple
-        onClick={props.onClick}
-        color={props.type === 'empty'
-          ? `var(--lightened-${color()})`
-          : undefined}
-        style={{ display: 'inline-block' }}
-      >
-        <button
-          class={props.class}
-          type='button'
-          style={props.style}
-          classList={{
-            'primary': color() === 'primary',
-            'secondary': color() === 'secondary',
-            'tertiary': color() === 'tertiary',
+        'empty': props.type === 'empty',
+        'gray-0': depth() === 1,
+        'gray-1': depth() === 0 || depth() === 2,
+        'gray-2': depth() === 3,
+        'gray-3': depth() === 4,
 
-            'disabled': props.disabled,
+        'small': props.size === 'small',
+        'medium': props.size === 'medium' || typeof props.size === 'undefined',
+        'large': props.size === 'large',
 
-            'empty': props.type === 'empty',
-            'gray-0': depth() === 1,
-            'gray-1': depth() === 0 || depth() === 2,
-            'gray-2': depth() === 3,
-            'gray-3': depth() === 4,
-
-            'small': props.size === 'small',
-            'medium': props.size === 'medium' || typeof props.size === 'undefined',
-            'large': props.size === 'large',
-
-            ...props.classList
-          }}
-        >{props.children}</button>
-      </Ripple>}
-  </>);
+        ...props.classList
+      }}
+    >{props.children}</button>
+  </Ripple>;
 }
 
 export default Button;
