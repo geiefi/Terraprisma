0f753c0f09035b3a13ac9a966df9e996bcafa201
gabriel miranda <gabrielmfern@outlook.com>
2023-05-20 10:23:17 -0300

    fix: add classList and class into the Stack component

--- packages/grapes/src/Layout/Stack/Stack.tsx
+++ packages/grapes/src/Layout/Stack/Stack.tsx
@@ -8,18 +8,21 @@ export type StackProps = {
     | 'space-around' | 'space-evenly';
   direction?: 'horizontal' | 'vertical';
   fullWidth?: boolean;
+  class?: string;
+  classList?: Record<string, boolean | undefined>;
   style?: JSX.CSSProperties;
 };
 
 const Stack: Component<ParentProps<StackProps>> = (props) => {
   return <div 
-    class='stack'
+    class={'stack ' + (props.class | undefined)}
     style={{ gap: `${props.spacing}px`, "justify-content": props.align, ...props.style }}
     classList={{
       'full-width': props.fullWidth,
       'horizontal': props.direction === 'horizontal' 
         || typeof props.direction === 'undefined',
-      'vertical': props.direction === 'vertical'
+      'vertical': props.direction === 'vertical',
+      ...props.classList
     }}
   >
     {props.children}
