29dec63caa63e32a61b3046f9de8043a5c3fe74d
gabriel miranda <gabrielmfern@outlook.com>
2023-05-22 08:06:25 -0300

    feature: add direction into the Divisor component

--- packages/grapes/src/Layout/Divisor/Divisor.tsx
+++ packages/grapes/src/Layout/Divisor/Divisor.tsx
@@ -3,6 +3,8 @@ import type { Component, JSX } from "solid-js";
 import './Divisor.scss';
 
 export type DivisorProps = {
+  direction?: 'vertical' | 'horizontal';
+
   style?: JSX.CSSProperties,
   class?: string,
   classList?: Record<string, boolean | undefined>
@@ -11,7 +13,10 @@ export type DivisorProps = {
 const Divisor: Component<DivisorProps> = (props) => {
   return <div 
     class={`divisor ${props.class || ''}`} 
-    classList={props.classList}
+    classList={{
+      'vertical': props.direction === 'vertical',
+      ...props.classList
+    }}
     style={props.style}
   ></div>;
 };
