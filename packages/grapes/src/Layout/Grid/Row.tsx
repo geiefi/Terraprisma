f0ec9225f7bbb1cd63df54e1fd1da66127976db6
gabriel miranda <gabrielmfern@outlook.com>
2023-05-22 08:06:10 -0300

    feature: add class, classList and style into the Row component

--- packages/grapes/src/Layout/Grid/Row.tsx
+++ packages/grapes/src/Layout/Grid/Row.tsx
@@ -1,14 +1,26 @@
-import type { Component, ParentProps } from "solid-js";
+import type { Component, JSX, ParentProps } from "solid-js";
 
 import './Row.scss';
 
 export type RowProps = ParentProps<{
   gap?: string;
   rowGap?: string;
+
+  class?: string;
+  classList?: Record<string, boolean | undefined>;
+  style?: JSX.CSSProperties;
 }>;
 
 const Row: Component<RowProps> = (props) => {
-  return <div class='row' style={{ gap: props.gap, 'row-gap': props.rowGap }}>
+  return <div 
+    class={'row ' + (props.class || '')}
+    classList={props.classList}
+    style={{ 
+      gap: props.gap, 
+      'row-gap': props.rowGap,
+      ...props.style
+    }}
+  >
     {props.children}
   </div>;
 };
