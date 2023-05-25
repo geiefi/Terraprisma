c28635af22319b3e53186d8a95cbbe1e0f6ae1c7
gabriel miranda <gabrielmfern@outlook.com>
2023-05-25 18:38:09 -0300

    feature: add class and classList to the props of the Col

--- packages/grapes/src/Layout/Grid/Col.tsx
+++ packages/grapes/src/Layout/Grid/Col.tsx
@@ -3,11 +3,16 @@ import type { Component, JSX, ParentProps } from "solid-js";
 export type ColProps = ParentProps<{
   size: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21
     | 22 | 23 | 24;
+
+  class?: string;
+  classList?: Record<string, boolean | undefined>;
   style?: JSX.CSSProperties;
 }>;
 
 const Col: Component<ColProps> = (props) => {
   return <div 
+    class={props.class}
+    classList={props.classList}
     style={{ 
       width: `${props.size / 24 * 100}%`,
       ...props.style
