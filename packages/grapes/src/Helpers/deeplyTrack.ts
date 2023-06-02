52162d484a64c084aa9a97a421bcad73a7456a07
gabriel miranda <gabrielmfern@outlook.com>
2023-06-02 09:29:50 -0300

    feature (Helpers): add a deeplyTrack helper function that goes recursively through all keys of an obj

---
+++ packages/grapes/src/Helpers/deeplyTrack.ts
@@ -0,0 +1,16 @@
+/**
+  * @description Accesses all the internal values of an item to make sure it is being tracked by
+  * createMemo, createEffect, etc.
+  * Very useful for stores.
+  */
+export function deeplyTrack(item: any, ignore: string[]=[]): void {
+  if (typeof item === 'object') {
+    for (const key of Object.keys(item)) {
+      if (ignore.includes(key)) {
+        deeplyTrack(item[key]);
+      }
+    }
+  } else {
+    item;
+  }
+}
