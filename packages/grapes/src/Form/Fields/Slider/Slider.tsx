9b77997f24a1eaaa0ac99cd5b8fb042964834463
gabriel miranda <gabrielmfern@outlook.com>
2023-06-03 20:05:35 -0300

    feature: Basic implementation of a Slider component

---
+++ packages/grapes/src/Form/Fields/Slider/Slider.tsx
@@ -0,0 +1,139 @@
+import { Component, JSX, onCleanup, onMount, Show, splitProps } from 'solid-js';
+
+import { FieldPropKeys, FieldProps, setupField } from '../_Shared/Utilts';
+
+import { FieldInternalWrapper } from '../_Shared';
+
+import './Slider.scss';
+import { FormValue } from '../../FormContext';
+
+export interface SliderProps extends FieldProps, Omit<JSX.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'name' | 'value'> {
+  label?: JSX.Element;
+  helperText?: JSX.Element;
+
+  color?: 'primary' | 'secondary' | 'tertiary';
+
+  onChange?: (value: number) => void,
+  onFocus?: () => void,
+}
+
+const Slider: Component<SliderProps> = (allProps) => {
+  const [props, elProps] = splitProps(
+    allProps,
+    [...FieldPropKeys, 'label', 'helperText', 'color', 'onChange', 'onFocus']
+  );
+
+  const {
+    elementId: id,
+    errorsStore: [errors],
+    disabledSignal: [disabled],
+    focusedSignal: [focused, setFocused],
+    valueSignal: [value, setValue],
+    validate,
+  } = setupField<SliderProps, FormValue, number>(props, 0);
+
+  // eslint-disable-next-line prefer-const
+  let slider: HTMLDivElement = undefined as any;
+
+  const updateValueBasedOnMouseX = (mouseX: number) => {
+    const mouseRelativeX = mouseX - slider.getBoundingClientRect().x;
+
+    const newValue = Math.max(Math.min(mouseRelativeX / slider.clientWidth * 100, 100), 0);
+    setValue(newValue);
+
+    if (props.onChange) {
+      props.onChange(newValue);
+    }
+  };
+
+  const handleMouseDown = (e: MouseEvent) => {
+    if (!slider.contains(e.target as HTMLElement) && slider !== e.target) return;
+    setFocused(true);
+    updateValueBasedOnMouseX(e.x);
+  };
+  const handleMouseUp = (e: MouseEvent) => {
+    if (focused()) {
+      updateValueBasedOnMouseX(e.x);
+      setFocused(false);
+    }
+  };
+  const handleMouseMove = (e: MouseEvent) => {
+    if (focused()) {
+      updateValueBasedOnMouseX(e.x);
+    }
+  };
+  onMount(() => {
+    slider.addEventListener('mousedown', handleMouseDown, { passive: true });
+    document.addEventListener('mousemove', handleMouseMove, { passive: true });
+    document.addEventListener('mouseup', handleMouseUp);
+  });
+
+  onCleanup(() => {
+    slider.removeEventListener('mousedown', handleMouseDown);
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+  });
+
+  return <FieldInternalWrapper
+    name={props.name}
+    errors={errors}
+    helperText={props.helperText}
+    renderHelperText={
+      (typeof props.validators !== 'undefined'
+        && props.validators.length !== 0)
+      || typeof props.helperText !== 'undefined'
+    }
+    isDisabled={disabled()}
+  >
+    <Show when={props.label}>
+      <label for={id()} class="label">{props.label}</label>
+    </Show>
+
+    <div
+      class='slider'
+      ref={slider}
+      draggable={false}
+      classList={{
+        focused: focused(),
+
+        'has-label': typeof props.label !== 'undefined',
+
+        primary: props.color === 'primary' || typeof props.color === 'undefined',
+        secondary: props.color === 'secondary',
+        tertiary: props.color === 'tertiary'
+      }}
+      style={{
+        '--slider-value': `${value() || 0}%`,
+      }}
+    >
+      <span 
+        class='trunk' 
+        draggable={false}
+      />
+      <span 
+        class='rail' 
+        draggable={false}
+      />
+      <span 
+        class='thumb'
+        draggable={false}
+      >
+        <input
+          min={0}
+          max={100}
+          step={1}
+
+          {...elProps}
+
+          id={id()}
+          value={(value() || '').toString()}
+
+          type='range'
+          disabled={disabled()}
+        />
+      </span>
+    </div>
+  </FieldInternalWrapper>;
+};
+
+export default Slider;
--- packages/grapes/src/Form/Fields/index.ts
+++ packages/grapes/src/Form/Fields/index.ts
@@ -3,5 +3,6 @@ import Select from "./Select/Select";
 import ButtonChooser from "./ButtonChooser/ButtonChooser";
 import TextArea from "./TextArea/TextArea";
 import Datepicker from "./Datepicker/Datepicker";
+import Slider from "./Slider/Slider";
 
-export { Input, Select, ButtonChooser, TextArea, Datepicker };
+export { Input, Slider, Select, ButtonChooser, TextArea, Datepicker };
