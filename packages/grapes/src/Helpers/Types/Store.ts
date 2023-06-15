06a6a1b2c76fff1945993934a1fffaeb198afb6f
gabriel miranda <gabrielmfern@outlook.com>
2023-06-15 17:12:51 -0300

    chore: add a way to build the GrapeS library to be npm published later

--- packages/grapes/src/Helpers/Types/Store.ts
+++ packages/grapes/src/Helpers/Types/Store.ts
@@ -1,3 +1,4 @@
 import { SetStoreFunction } from 'solid-js/store';
 
 export type Store<T> = [get: T, set: SetStoreFunction<T>];
+
---
+++ packages/grapes/src/Helpers/index.ts
@@ -0,0 +1,9 @@
+import { deeplyTrack } from './deeplyTrack';
+import { splitTupleAccessor } from './splitTupleAccessor';
+
+import type { Store } from './Types/Store';
+
+export { deeplyTrack, splitTupleAccessor };
+
+export type { Store };
+
