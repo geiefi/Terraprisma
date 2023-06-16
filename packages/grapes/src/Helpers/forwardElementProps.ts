1a77fa3520ba63137a0b4279c85f461a64af4d6b
gabriel miranda <gabrielmfern@outlook.com>
2023-06-16 11:06:37 -0300

    chore (forwardNativeElementProps): improve type inference for the componentFunc param

--- packages/grapes/src/Helpers/forwardElementProps.ts
+++ packages/grapes/src/Helpers/forwardElementProps.ts
@@ -2,10 +2,10 @@ import { Component, splitProps } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 
 export function forwardNativeElementProps<Props extends Record<string, any>, ElementType extends HTMLElement>(
-  componentFunc: (props: Props, elProps: JSX.HTMLAttributes<ElementType>) => JSX.Element | JSX.Element[],
+  componentFunc: (props: Props, elProps: Omit<JSX.HTMLAttributes<ElementType>, keyof Props>) => JSX.Element | JSX.Element[],
   componentPropNames: [keyof Props] | (keyof Props)[],
-): Component<JSX.HTMLAttributes<ElementType> & Props> {
-  return (allProps: JSX.HTMLAttributes<ElementType> & Props) => componentFunc(
+): Component<Props & JSX.HTMLAttributes<ElementType>> {
+  return (allProps: Props & JSX.HTMLAttributes<ElementType>) => componentFunc(
     ...splitProps(allProps, componentPropNames)
   );
 }
