023941e6583b75b39f851ddb630aa9832388d990
gabriel miranda <gabrielmfern@outlook.com>
2023-06-16 10:10:41 -0300

    feature (Helpers): implement a mergeCallbacks function to use with elements that inherit native props

--- packages/grapes/src/Helpers/index.ts
+++ packages/grapes/src/Helpers/index.ts
@@ -1,9 +1,11 @@
 import { deeplyTrack } from './deeplyTrack';
 import { splitTupleAccessor } from './splitTupleAccessor';
+import { forwardNativeElementProps } from './forwardElementProps';
+import { mergeCallbacks } from './mergeCallbacks';
 
 import type { Store } from './Types/Store';
 
-export { deeplyTrack, splitTupleAccessor };
+export { deeplyTrack, splitTupleAccessor, forwardNativeElementProps as forwardElementProps, mergeCallbacks };
 
 export type { Store };
 
---
+++ packages/grapes/src/Helpers/mergeCallbacks.ts
@@ -0,0 +1,20 @@
+type Func = (...args: any[]) => any;
+
+export function mergeCallbacks<Callback extends Func>(...callbacks: [...(Callback | undefined)[], Callback]): Callback {
+  return ((...args: any[]) => {
+    for (const callback of callbacks.slice(0, -1)) {
+      if (typeof callback === 'function') {
+        callback(...args);
+      }
+    }
+
+    const lastCallback = callbacks[callbacks.length - 1];
+    if (typeof lastCallback === 'function') {
+      return lastCallback(...args);
+    } else {
+      throw new Error(
+        'It seems the last callback is not a function, but it is required since it determines the return of the merged callbacks!'
+      );
+    }
+  }) as Callback;
+}
