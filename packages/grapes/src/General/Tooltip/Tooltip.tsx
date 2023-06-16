c2a61a4a40a5cfaa524ed6f85e958bf69f4450ca
gabriel miranda <gabrielmfern@outlook.com>
2023-06-16 11:38:17 -0300

    feature (Tooltip): finish Tooltip implementation by allowing for different positionings and using the forwardNativeElementProps

--- packages/grapes/src/General/Tooltip/Tooltip.tsx
+++ packages/grapes/src/General/Tooltip/Tooltip.tsx
@@ -1,15 +1,23 @@
-import { Component, createMemo, JSX, on, Show, splitProps } from 'solid-js';
+import {
+  Component,
+  createMemo,
+  JSX,
+  on,
+  ParentProps,
+  Show,
+} from 'solid-js';
 import { mergeClass } from '../../_Shared/Utils';
 
 import './Tooltip.scss';
+import { forwardNativeElementProps } from '../../Helpers/forwardElementProps';
 
-export interface TooltipProps extends JSX.HTMLAttributes<HTMLDivElement> {
+export interface TooltipProps extends ParentProps {
   /**
    * @description The element or bounding box the Tooltip will be anchored to.
    *
    * This is required because it is used to position the tooltip correctly.
    *
-   * The bounding box can be used when there is a need to reactively update the 
+   * The bounding box can be used when there is a need to reactively update the
    * positioning of the tooltip based on the anchor and the ref doesn't really update.
    *
    * For example the `<Slider>` component uses this and it has an effect to reset
@@ -31,55 +39,54 @@ export interface TooltipProps extends JSX.HTMLAttributes<HTMLDivElement> {
   style?: JSX.CSSProperties;
 }
 
-const Tooltip: Component<TooltipProps> = (allProps) => {
-  const [props, elProps] = splitProps(
-    allProps,
-    ['for', 'visible', 'offsetFromAnchor', 'position']
-  );
-
-  // eslint-disable-next-line solid/reactivity
-  const boundingRect = createMemo(
-    on(
-      [
-        () => props.visible,
-        () => props.for
-      ],
-      () => {
+const Tooltip: Component<TooltipProps> = forwardNativeElementProps<
+  TooltipProps,
+  HTMLDivElement
+>(
+  (props, elProps) => {
+    const boundingRect = createMemo(
+      on([() => props.visible, () => props.for], () => {
         if (props.for instanceof HTMLElement) {
           return props.for.getBoundingClientRect();
         } else {
           return props.for;
         }
-      }
-    )
-  );
+      })
+    );
 
-  return <Show when={props.visible}>
-    <div
-      {...elProps}
-      class={mergeClass('tooltip', elProps.class)}
-      style={{
-        '--anchor-left': `${boundingRect()?.x}px`,
-        '--anchor-top': `${boundingRect()?.y}px`,
-        '--anchor-width': `${boundingRect()?.width}px`,
-        '--anchor-height': `${boundingRect()?.height}px`,
+    let tooltipEl: HTMLDivElement;
 
-        '--offset-from-anchor': props.offsetFromAnchor || '5px',
+    return (
+      <Show when={props.visible}>
+        <div
+          {...elProps}
+          class={mergeClass('tooltip', elProps.class)}
+          ref={tooltipEl!}
+          style={{
+            '--anchor-left': `${boundingRect()?.x}px`,
+            '--anchor-top': `${boundingRect()?.y}px`,
+            '--anchor-width': `${boundingRect()?.width}px`,
+            '--anchor-height': `${boundingRect()?.height}px`,
 
-        ...elProps.style,
-      }}
-      classList={{
-        'top': props.position === 'top' || typeof props.position === 'undefined',
-        'bottom': props.position === 'bottom',
-        'left': props.position === 'left',
-        'right': props.position === 'right',
-        ...elProps.classList
-      }}
-    >
-      {elProps.children}
-      {/*<ArrowDropDown variant="sharp" />*/}
-    </div>
-  </Show>;
-};
+            '--offset-from-anchor': props.offsetFromAnchor || '5px',
+
+            ...props.style,
+          }}
+          classList={{
+            top:
+              props.position === 'top' || typeof props.position === 'undefined',
+            bottom: props.position === 'bottom',
+            left: props.position === 'left',
+            right: props.position === 'right',
+            ...elProps.classList,
+          }}
+        >
+          {props.children}
+        </div>
+      </Show>
+    );
+  },
+  ['for', 'visible', 'offsetFromAnchor', 'position', 'children', 'style']
+);
 
 export default Tooltip;
