96543750163774d74a988df280ee1518e4aaddce
gabriel miranda <gabrielmfern@outlook.com>
2023-07-30 09:42:09 -0300

    fix (forwardNativeElementProps): make the resulting component's props take the normal props as priority instead of the element's

--- packages/grapos/src/Helpers/forwardNativeElementProps.ts
+++ packages/grapos/src/Helpers/forwardNativeElementProps.ts
@@ -4,12 +4,19 @@ import { JSX } from 'solid-js/jsx-runtime';
 export function forwardNativeElementProps<
 Props extends Record<string, any>, 
 ElementType extends HTMLElement, 
-ElementProps = JSX.HTMLAttributes<ElementType>
+PropsToIgnore extends (keyof Props)[] = (keyof Props)[],
+ElementProps extends JSX.HTMLAttributes<ElementType> 
+  = JSX.HTMLAttributes<ElementType>,
+ResultingComponentProps extends Props & Omit<ElementProps, PropsToIgnore[number]> 
+  = Props & Omit<ElementProps, PropsToIgnore[number]>,
 >(
-  componentFunc: (props: Props, elProps: Omit<ElementProps, keyof Props>) => JSX.Element | JSX.Element[],
-  componentPropNames: (keyof Props)[],
-): Component<Props & ElementProps> {
-  return (allProps: Props & ElementProps) => componentFunc(
-    ...splitProps(allProps, componentPropNames)
+  componentFunc: (
+    props: Props, 
+    elProps: Omit<ElementProps, PropsToIgnore[number]>
+  ) => JSX.Element,
+  componentPropNames: PropsToIgnore,
+): Component<ResultingComponentProps> {
+  return (allProps: ResultingComponentProps) => componentFunc(
+    ...(splitProps(allProps, componentPropNames) as unknown as [Props, Omit<ElementProps, PropsToIgnore[number]>])
   );
 }
