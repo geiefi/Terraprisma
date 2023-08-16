67152e76e5983b8bab38ba4c7af96d50cf9e21e9
gabriel miranda <gabrielmfern@outlook.com>
2023-08-16 10:46:26 -0300

    feature: create a Collapse transition

---
+++ packages/grapos/src/Transitions/Collapse/Collapse.tsx
@@ -0,0 +1,19 @@
+import { type Component, type ParentProps } from 'solid-js';
+import { Transition, TransitionProps } from 'solid-transition-group';
+
+import './GrowFade.scss';
+
+export interface CollapseProps extends Omit<TransitionProps, 'name'>, ParentProps {
+  /**
+   * @default 'top'
+   */
+  origin?: 'top' | 'left' | 'bottom' | 'right';
+}
+
+const Collapse: Component<CollapseProps> = (props) => (
+  <Transition name={`collapse-${props.origin || 'top'}`} {...props}>
+    {props.children}
+  </Transition>
+);
+
+export default Collapse;
