c3d622f979a46eeac446e8cb9dc44fcfe9025de5
gabriel miranda <gabrielmfern@outlook.com>
2023-08-16 10:46:17 -0300

    refactor: make use of a for each instead of repeating code for the GrowFade transition

--- packages/grapos/src/Transitions/index.ts
+++ packages/grapos/src/Transitions/index.ts
@@ -1,4 +1,5 @@
 import GrowFade from './GrowFade/GrowFade'
 import Fade from './Fade/Fade';
+import Collapse from './Collapse/Collapse';
 
-export { GrowFade, Fade };
+export { GrowFade, Fade, Collapse };
