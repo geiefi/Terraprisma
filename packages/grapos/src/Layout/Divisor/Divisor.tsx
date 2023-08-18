55b3c632e7c1c472030684051ab7edb6862baba4
gabriel miranda <gabrielmfern@outlook.com>
2023-08-18 20:56:02 -0300

    chore: use forwardNativeElementProps for the Divisor component

--- packages/grapos/src/Layout/Divisor/Divisor.tsx
+++ packages/grapos/src/Layout/Divisor/Divisor.tsx
@@ -2,22 +2,29 @@ import { Component, JSX, splitProps } from 'solid-js';
 import { mergeClass } from '../../_Shared/Utils';
 
 import './Divisor.scss';
+import { forwardNativeElementProps } from '../../Helpers';
 
-export interface DivisorProps extends JSX.HTMLAttributes<HTMLDivElement> {
+export interface DivisorProps {
   direction?: 'vertical' | 'horizontal';
 }
 
-const Divisor: Component<DivisorProps> = (allProps) => {
-  const [props, elProps] = splitProps(allProps, ['direction']);
-
-  return <div 
-    {...elProps}
-    class={mergeClass('divisor', elProps.class)} 
-    classList={{
-      'vertical': props.direction === 'vertical',
-      ...elProps.classList
-    }}
-  />;
-};
+const Divisor: Component<DivisorProps> = forwardNativeElementProps<
+  DivisorProps,
+  HTMLDivElement
+>(
+  (props, elProps) => {
+    return (
+      <div
+        {...elProps}
+        class={mergeClass('divisor', elProps.class)}
+        classList={{
+          vertical: props.direction === 'vertical',
+          ...elProps.classList,
+        }}
+      />
+    );
+  },
+  ['direction']
+);
 
 export default Divisor;
