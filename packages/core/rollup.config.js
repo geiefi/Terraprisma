52985b30b1b970d58d39a69a084d41dae8219f49
gabriel miranda <gabrielmfern@outlook.com>
2023-08-29 18:02:32 -0300

    feature: add a require option for the GrapeS packages

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -10,45 +10,52 @@ import jsx from 'acorn-jsx';
 let nextId = 0;
 
 function getJsxName(node) {
-  if (node.type === "JSXMemberExpression") {
-    return `${getJsxName(node.object)}.${getJsxName(node.property)}`
+  if (node.type === 'JSXMemberExpression') {
+    return `${getJsxName(node.object)}.${getJsxName(node.property)}`;
   }
-  return node.name
+  return node.name;
 }
 
 /** @type {import('rollup').PluginImpl} */
 function jsxPreserve() {
   return {
-    name: "jsxPreserve",
+    name: 'jsxPreserve',
 
     transform(code, fileId) {
       if (fileId.endsWith('.tsx')) {
-        const magicString = new MagicString(code)
-        const idsByName = new Map()
-        const tree = this.parse(code)
+        const magicString = new MagicString(code);
+        const idsByName = new Map();
+        const tree = this.parse(code);
         walk(tree, {
           enter(node) {
-            if (node.type === "JSXOpeningElement" || node.type === "JSXClosingElement") {
-              const name = getJsxName(node.name)
-              const tagId = idsByName.get(name) || `JSX_ID_${nextId += 1}`
+            if (
+              node.type === 'JSXOpeningElement' ||
+              node.type === 'JSXClosingElement'
+            ) {
+              const name = getJsxName(node.name);
+              const tagId = idsByName.get(name) || `JSX_ID_${(nextId += 1)}`;
 
               // overwrite all JSX tags with artificial tag ids so that we can find them again later
-              magicString.overwrite(node.name.start, node.name.end, tagId)
-              idsByName.set(name, tagId)
+              magicString.overwrite(node.name.start, node.name.end, tagId);
+              idsByName.set(name, tagId);
             }
             // do not treat the children as separate identifiers
-            else if (node.type === "JSXMemberExpression") {
-              this.skip()
+            else if (node.type === 'JSXMemberExpression') {
+              this.skip();
             }
           }
-        })
+        });
 
         if (idsByName.size > 0) {
-          const usedNamesAndIds = [...idsByName].map(([name, tagId]) => `/*${tagId}*/${name}`)
-          magicString.append(`\n__FAKE_FUNC_USED_JSX_NAMES__(${usedNamesAndIds.join(',')});`);
+          const usedNamesAndIds = [...idsByName].map(
+            ([name, tagId]) => `/*${tagId}*/${name}`
+          );
+          magicString.append(
+            `\n__FAKE_FUNC_USED_JSX_NAMES__(${usedNamesAndIds.join(',')});`
+          );
           return {
             code: magicString.toString(),
-            map: magicString.generateMap(),
+            map: magicString.generateMap()
           };
         }
 
@@ -66,20 +73,24 @@ function jsxPreserve() {
         // and collects the new variable names as a side-effect
         .replace(/__FAKE_FUNC_USED_JSX_NAMES__\(([^)]*)\);/g, (_, usedList) => {
           usedList
-            .split(",")
+            .split(',')
             .map((replacementAndVariable) =>
               replacementAndVariable.match(/^\s*?\/\*([^*]*)\*\/\s*?(\S*)$/)
             )
             .filter(Boolean)
-            .forEach(([usedEntry, tagId, updatedName]) => replacements.set(tagId, updatedName))
+            .forEach(([usedEntry, tagId, updatedName]) =>
+              replacements.set(tagId, updatedName)
+            );
 
           // clearing out the actual values
-          return "";
+          return '';
         });
       return {
-        code: codeWithRemovedFakeFunction.replace(/JSX_ID_\d+/g, (tagId) => replacements.get(tagId)),
+        code: codeWithRemovedFakeFunction.replace(/JSX_ID_\d+/g, (tagId) =>
+          replacements.get(tagId)
+        ),
         map: null
-      }
+      };
     }
   };
 }
@@ -87,10 +98,16 @@ function jsxPreserve() {
 /** @type {import('rollup').RollupOptions} */
 export const baseConfig = {
   input: './src/index.ts',
-  output: {
-    file: './lib/index.jsx',
-    format: 'esm'
-  },
+  output: [
+    {
+      file: './lib/index.esm.jsx',
+      format: 'es'
+    },
+    {
+      file: './lib/index.cjs.jsx',
+      format: 'cjs'
+    }
+  ],
   acornInjectPlugins: [jsx()],
   plugins: [
     typescript({
@@ -98,8 +115,8 @@ export const baseConfig = {
       declarationDir: './lib/'
     }),
     jsxPreserve(),
-    styles(),
-  ],
+    styles()
+  ]
 };
 
 export default defineConfig(baseConfig);
