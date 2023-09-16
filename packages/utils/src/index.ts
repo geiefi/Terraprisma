c8f710c77ada402b857f893d3d0c4e63a1f236a4
gabriel miranda <gabrielmfern@outlook.com>
2023-09-16 13:46:29 -0300

    fix (packages/utils): adjust exports and move the dbg function into it

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,18 +1,38 @@
 import { deeplyTrack } from './deeplyTrack';
 import { splitTupleAccessor } from './splitTupleAccessor';
-import { createComponentExtendingFromOther } from './createComponentExtendingFromOther';
 import { mergeCallbacks } from './mergeCallbacks';
 import { mergeClass } from './mergeClass';
 import { canUseDocument } from './canUseDocument';
 
+export * from './factories';
 export * from './types';
 
 import type { Store } from './types/Store';
 
+declare global {
+  function dbg<T = any>(el: T, context?: string): T;
+}
+
+const _global = typeof window !== 'undefined' ? window : global;
+/**
+ * @description `console.log`'s the `el` then returns it
+ *
+ * This function is inspired in Rust's `dbg!` macro.
+ */
+_global.dbg = function <T = any>(el: T, context?: string): T {
+  if (context) {
+    console.groupCollapsed(`${context}:`, el);
+  } else {
+    console.groupCollapsed(el);
+  }
+  console.trace('for this dbg() call');
+  console.groupEnd();
+  return el;
+};
+
 export {
   deeplyTrack,
   splitTupleAccessor,
-  createComponentExtendingFromOther,
   mergeClass,
   mergeCallbacks,
   canUseDocument
