293e9118c77fda7295943232fa67c818c7af100d
gabriel miranda <gabrielmfern@outlook.com>
2023-09-19 14:39:40 -0300

    fix (box): adjust border to be 4px according to the figma design

--- packages/core/src/Box/Box.tsx
+++ packages/core/src/Box/Box.tsx
@@ -6,20 +6,8 @@ import './Box.scss';
 export interface BoxProps extends ParentProps {}
 
 /**
- * @description A component used for having a kind of box, this Box creates a context automatically
- * that communicates to other boxes inside the depth that they should have automatically changing
- * their color accordingly.
- *
- * @example
- * ```tsx
- * <Box>
- *   <h1>Box with depth 1</h1>
- *
- *   <Box>
- *     <h2>Box with depth 2</h2>
- *   </Box>
- * </Box>
- * ```
+ * @description A component used for having a kind of box.
+ * It uses the `floating.bg` and `floating.fg` for its *background* and *text* color respectively.
  */
 const Box = makeComponent(
   [extendPropsFrom<BoxProps, 'div'>(['children'])],
