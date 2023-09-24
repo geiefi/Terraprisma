c121ac2ab8513b9b42873bf2ce2e0bee06892a54
gabriel miranda <gabrielmfern@outlook.com>
2023-09-24 15:21:38 -0300

    feature (theming): generate a .1 and .3 opacity variant for all colors

--- packages/theming/src/generateStyleVariablesFrom.ts
+++ packages/theming/src/generateStyleVariablesFrom.ts
@@ -14,13 +14,16 @@ export function generateStyleVariablesFrom(
       const kebabCaseProperty = toKebabCase(property);
 
       const stylesForValue: Record<string, string> = {};
+      const varName = `${[prefix, kebabCaseProperty]
+        .filter(Boolean)
+        .join('-')}`;
       if (Array.isArray(value)) {
         value
           .filter((l) => l instanceof Color)
           .forEach((v: Color, i) => {
-            stylesForValue[
-              `--${[prefix, kebabCaseProperty].filter(Boolean).join('-')}-${i}`
-            ] = v.toRGBA();
+            stylesForValue[`--${varName}-${i}`] = v.toRGBA();
+            stylesForValue[`--${varName}-${i}/10`] = v.withAlpha(0.1).toRGBA();
+            stylesForValue[`--${varName}-${i}/30`] = v.withAlpha(0.3).toRGBA();
           });
       } else if (typeof value === 'object' && !(value instanceof Color)) {
         const valueStyles = generateStyleVariablesFrom(
@@ -31,9 +34,9 @@ export function generateStyleVariablesFrom(
           (innerKey) => (stylesForValue[innerKey] = valueStyles[innerKey])
         );
       } else if (value instanceof Color) {
-        stylesForValue[
-          `--${[prefix, kebabCaseProperty].filter(Boolean).join('-')}`
-        ] = value.toRGBA();
+        stylesForValue[`--${varName}`] = value.toRGBA();
+        stylesForValue[`--${varName}/10`] = value.withAlpha(0.1).toRGBA();
+        stylesForValue[`--${varName}/30`] = value.withAlpha(0.3).toRGBA();
       }
       return stylesForValue;
     })
