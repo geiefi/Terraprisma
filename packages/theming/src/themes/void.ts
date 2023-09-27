e3f774dd49e37791c7e05024d167359f6b097a63
gabriel miranda <gabrielmfern@outlook.com>
2023-09-27 13:16:49 -0300

    fix (theming): improve the hover for the accent in the void theme

--- packages/theming/src/themes/void.ts
+++ packages/theming/src/themes/void.ts
@@ -3,12 +3,12 @@ import { Color, Theme, cbf, cbfb, cbfh } from '../Theme';
 export const VoidTheme = {
   id: 'void',
 
-  bgBackdrop: new Color('rgba(0,0,0,.4)'),
+  bgBackdrop: new Color('#000000').withAlpha(0.4),
   normal: cbf('#000000', '#ffffff'),
   deeper: cbf('#151515', '#ffffff'),
   floating: cbfb('#000000', '#ffffff', '#303030'),
 
-  accent: cbfh('#ffffff', '#000000', '#A9A9A9'),
+  accent: cbfh('#ffffff', '#000000', '#CCCACA'),
   muted: cbf('#6B6B6B', '#898989'),
   marked: cbf('#4555A2', '#ffffff'),
 
