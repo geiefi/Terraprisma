4146b1629dc215454d1da0b803798872e387bea4
gabriel miranda <gabrielmfern@outlook.com>
2023-10-12 11:21:09 -0300

    fix (utils): adjust mergeCallback to work properly with bound events

--- packages/utils/src/mergeCallbacks.ts
+++ packages/utils/src/mergeCallbacks.ts
@@ -1,27 +1,58 @@
-type Func =
-  | ((...args: any[]) => any)
-  | {
-      (...args: any[]): any;
-    }
-  | Event;
+type BoundCallback<Data, Args extends any[], ReturnType> = {
+  1: Data;
+  0: (data: Data, ...args: Args) => ReturnType;
+};
 
-export function mergeCallbacks<Callback extends Func>(
-  ...callbacks: [...(Callback | undefined)[], Callback]
-): Callback {
-  return ((...args: any[]) => {
-    for (const callback of callbacks.slice(0, -1)) {
-      if (typeof callback === 'function') {
-        callback(...args);
-      }
+type Callback<Args extends any[], ReturnType = void> =
+  | ((...args: Args) => ReturnType)
+  | BoundCallback<any, Args, ReturnType>;
+
+function isCallbackBound<Args extends any[], ReturnType>(
+  callback: Callback<Args, ReturnType> | undefined
+): callback is BoundCallback<any, Args, ReturnType> {
+  return Array.isArray(callback) ? true : false;
+}
+
+/**
+ * @description Does the same as `mergeClass` but with callbacks.
+ * This allows for either a BoundCallback (see [here](https://www.solidjs.com/tutorial/bindings_events)) or a
+ * normal callback as a function with any parameters. Just like `mergeClass` it only considers truthy values, but will
+ * not allow the last one to not be truthy as that would break the callbacks.
+ *
+ * So that this function is more generic, the code will actually throw an error if the last function is undefined
+ * or not a function overall since it will be needed if the callbacks have a return type.
+ *
+ * @returns A new callback that will return the value of the last callback it was called with,
+ * and go through all of the callbcks proxying its received parameters into all of them.
+ *
+ * @example
+ * ```jsx
+ * const MyComponent = (props) => {
+ *   return <button onClick={mergeCallbacks(
+ *     () => console.log('clicked!'),
+ *     props.onClick,
+ *   )}>Click me man!</button>
+ * };
+ * ```
+ */
+export function mergeCallbacks<Args extends any[], ReturnType = void>(
+  ...callbacks: (Callback<Args, ReturnType> | undefined)[]
+): ((...args: Args) => ReturnType) | undefined {
+  const callbacksToCall = callbacks
+    .filter(Boolean)
+    .map((callback) =>
+      isCallbackBound(callback)
+        ? (...args: any) => callback[0](callback[1], ...args)
+        : callback!
+    );
+
+  if (callbacksToCall.length === 0) return;
+
+  return (...args: Args) => {
+    for (const callback of callbacksToCall.slice(0, -1)) {
+      callback(...args);
     }
 
-    const lastCallback = callbacks[callbacks.length - 1];
-    if (typeof lastCallback === 'function') {
-      return lastCallback(...args);
-    } else {
-      throw new Error(
-        'It seems the last callback is not a function, but it is required since it determines the return of the merged callbacks!'
-      );
-    }
-  }) as Callback;
+    return callbacksToCall[callbacksToCall.length - 1](...args);
+  };
 }
