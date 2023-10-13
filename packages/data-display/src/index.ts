dc81f4f6651416d54ca90c74dd3061635b65f743
gabriel miranda <gabrielmfern@outlook.com>
2023-10-13 15:51:52 -0300

    chore (data-display): remove List

--- packages/data-display/src/index.ts
+++ packages/data-display/src/index.ts
@@ -1,5 +1,4 @@
 import Tooltip, { createTooltip } from './Tooltip/Tooltip';
 import Table from './Table/Table';
-import List from './List/List';
 
-export { Table, Tooltip, createTooltip, List };
+export { Table, Tooltip, createTooltip };
