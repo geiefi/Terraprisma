af56446deab5e1101681fee7dd78d4041cfe6baf
gabriel miranda <gabrielmfern@outlook.com>
2023-11-09 02:24:58 -0300

    feat (docs): change the title of the website to Terraprisma

--- apps/documentation/src/root.tsx
+++ apps/documentation/src/root.tsx
@@ -32,7 +32,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Terraprisma</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
