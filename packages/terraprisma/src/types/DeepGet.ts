049bbf39f0af804d43b2f0351035e562430d10a0
gabriel miranda <gabrielmfern@outlook.com>
2023-12-06 04:17:01 -0300

    fix (DeepGet): not working on most cases due to it rendering into never for partial types

--- packages/terraprisma/src/types/DeepGet.ts
+++ packages/terraprisma/src/types/DeepGet.ts
@@ -1,7 +1,11 @@
-export type DeepGet<TValue, TPath> = TPath extends keyof TValue
+export type DeepGet<
+  _TValue,
+  TPath,
+  TValue extends Required<_TValue> = Required<_TValue>
+> = TPath extends keyof TValue
   ? TValue[TPath]
   : TPath extends `${infer FirstKey}.${infer Rest}`
-  ? FirstKey extends keyof TValue
-    ? DeepGet<TValue[FirstKey], Rest>
-    : never
-  : never;
+    ? FirstKey extends keyof TValue
+      ? DeepGet<TValue[FirstKey], Rest>
+      : never
+    : never;
