546a976cc25c651f48b348e14879235af4b229aa
gabriel miranda <gabrielmfern@outlook.com>
2024-02-01 21:11:54 -0300

    feat(docs): Add solid devtools's overlay

--- apps/terraprisma/src/entry-client.tsx
+++ apps/terraprisma/src/entry-client.tsx
@@ -1,3 +1,7 @@
 import { mount, StartClient } from "@solidjs/start/client";
+import 'solid-devtools';
+import { attachDevtoolsOverlay } from '@solid-devtools/overlay'
+
+attachDevtoolsOverlay()
 
 mount(() => <StartClient />, document.getElementById("app"));
--- apps/terraprisma/vite.config.ts
+++ apps/terraprisma/vite.config.ts
@@ -1,7 +1,13 @@
 import { searchForWorkspaceRoot } from 'vite';
+import devtools from 'solid-devtools/vite';
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  plugins: [
+    devtools({
+      autoname: true
+    })
+  ],
   server: {
     fs: {
       allow: [
