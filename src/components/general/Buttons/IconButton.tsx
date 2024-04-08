f8dc6229c5e90780ca332a63eb6e76b64b236f28
Fernanda Caroline <nandacarol123@gmail.com>
2024-04-08 17:36:25 -0300

    feat(IconButton): Improvd active class definition and default to squarish

--- src/components/general/Buttons/IconButton.tsx
+++ src/components/general/Buttons/IconButton.tsx
@@ -5,20 +5,12 @@ import { mergeClass } from '../../../utils';
 import { LeftIntersection } from '../../../types/LeftIntersection';
 
 export type IconButtonProps = LeftIntersection<
-  {
-    /**
-     * @default
-     */
-    squarish?: boolean;
-  },
+  {},
   ComponentProps<typeof TextButton>
 >;
 
 const IconButton = (allProps: IconButtonProps) => {
-  const [props, defaultButtonProps] = splitProps(allProps, [
-    'squarish',
-    'children'
-  ]);
+  const [props, defaultButtonProps] = splitProps(allProps, ['children']);
   return (
     <TextButton
       {...defaultButtonProps}
@@ -27,23 +19,23 @@ const IconButton = (allProps: IconButtonProps) => {
         contrastWithBg: true,
         ...defaultButtonProps.rippleProps
       }}
+      data-active={defaultButtonProps.active}
       unstyled
       class={mergeClass(
-        '!p-0',
+        'p-0 rounded-md',
         !defaultButtonProps.unstyled && [
           'aspect-square outline-none',
           (!defaultButtonProps.active || defaultButtonProps.disabled) &&
-          'bg-transparent text-[var(--bg)]',
+            'bg-transparent text-[var(--bg)]',
           !defaultButtonProps.disabled && [
             'hover:bg-[var(--bg)] hover:text-[var(--fg)]',
-            defaultButtonProps.active && 'bg-[var(--bg)] text-[var(--fg)]'
-          ],
-          props.squarish ? 'rounded-lg' : 'rounded-full'
+            'data-[active=true]:bg-[var(--bg)] data-[active=true]:text-[var(--fg)]'
+          ]
         ],
         defaultButtonProps.size === 'small' && 'w-5 h-5',
         (defaultButtonProps.size === 'medium' ||
           typeof defaultButtonProps.size === 'undefined') &&
-        'w-6 h-6',
+          'w-6 h-6',
         defaultButtonProps.size === 'large' && 'w-7 h-7',
         defaultButtonProps.class
       )}
@@ -54,4 +46,3 @@ const IconButton = (allProps: IconButtonProps) => {
 };
 
 export default IconButton;
-  
