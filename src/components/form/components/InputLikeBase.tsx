16eba7efce820ee73ea55766fcd9c16fc96ac984
Fernanda Caroline <nandacarol123@gmail.com>
2024-04-16 13:31:56 -0300

    feat(InputLikeBase): new paddings

--- src/components/form/components/InputLikeBase.tsx
+++ src/components/form/components/InputLikeBase.tsx
@@ -56,17 +56,17 @@ export const InputLikeBase = (allProps: InputLikeBaseProps) => {
       {...elProps}
       data-size={props.size ?? 'medium'}
       class={mergeClass(
-        'font-normal group box-border w-full data-[size=small]:min-h-[35px] data-[size=medium]:min-h-[44px] data-[size=large]:min-h-[70px] h-fit',
-        'relative m-0 data-[size=small]:px-3 data-[size=medium]:px-4 data-[size=large]:px-8',
+        'group box-border w-full data-[size=small]:min-h-11 data-[size=medium]:min-h-12 data-[size=large]:min-h-16 h-fit',
+        'relative m-0 data-[size=small]:px-4 data-[size=medium]:px-5 data-[size=large]:px-7',
         'bg-[var(--bg)] text-[var(--fg)] transition-colors',
-        'data-[size=small]:text-sm data-[size=medium]:text-base data-[size=large]:text-lg',
-        'data-[size=small]:rounded-lg data-[size=medium]:rounded-lg data-[size=large]:rounded-2xl',
+        'data-[size=small]:text-sm data-[size=medium]:text-base data-[size=large]:text-xl',
+        'data-[size=small]:rounded-xl data-[size=medium]:rounded-xl data-[size=large]:rounded-xl',
         '!outline-none border-solid border focus:focus-visible:border-[var(--color)] focus-visible:border-[var(--color)]',
         focused() ? 'border-[var(--color)]' : 'border-[var(--floating-border)]',
         disabled() && '!cursor-none',
-        props.label
-          ? 'data-[size=large]:pt-8 data-[size=large]:pb-3 data-[size=medium]:pt-5 data-[size=medium]:pb-2 data-[size=small]:pt-4 data-[size=small]:pb-1.5'
-          : 'data-[size=small]:py-2 data-[size=medium]:py-4 data-[size=large]:py-5',
+        props.label 
+          ? 'data-[size=small]:pt-6 data-[size=small]:pb-1.5 data-[size=medium]:pt-7 data-[size=medium]:pb-1.5 data-[size=large]:pt-8 data-[size=large]:pb-1.5'
+          : 'data-[size=small]:py-3 data-[size=medium]:py-3.5 data-[size=large]:py-4',
         elProps.class
       )}
       style={combineStyle(
@@ -81,10 +81,13 @@ export const InputLikeBase = (allProps: InputLikeBaseProps) => {
       <Show when={props.label}>
         <Label
           class={mergeClass(
-            'font-normal absolute origin-top-left transition-all text-inherit',
-            'text-ellipsis overflow-x-hidden group-data-[size=small]:left-3 group-data-[size=small]:w-[calc(100%-1rem)] group-data-[size=medium]:left-4 group-data-[size=medium]:w-[calc(100%-2rem)] group-data-[size=large]:left-8 group-data-[size=large]:w-[calc(100%-4rem)]',
+            'font-semibold absolute origin-top-left transition-all text-inherit',
+            'text-ellipsis overflow-x-hidden',
+            'group-data-[size=small]:left-4 group-data-[size=small]:w-[calc(100%-2*1rem)]',
+            'group-data-[size=medium]:left-5 group-data-[size=medium]:w-[calc(100%-2*1.25rem)]',
+            'group-data-[size=large]:left-7 group-data-[size=large]:w-[calc(100%-2*1.75rem)]',
             focused() || props.actLikeHasContent || hasContent()
-              ? 'group-data-[size=small]:top-1 group-data-[size=medium]:top-1.5 group-data-[size=large]:top-4 group-data-[size=medium]:scale-[0.642] group-data-[size=large]:scale-[0.428] opacity-70'
+              ? 'group-data-[size=small]:top-1 group-data-[size=medium]:top-2 group-data-[size=large]:top-1.5 group-data-[size=medium]:scale-[0.642] group-data-[size=large]:scale-[0.428] opacity-70'
               : 'group-data-[size=small]:top-4 group-data-[size=medium]:top-5 group-data-[size=large]:top-9 -translate-y-1/2'
           )}
           for={props.labelFor}
