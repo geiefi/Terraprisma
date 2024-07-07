05ad99688a0c9b595b66ee3c4196136c4c30b24c
gabriel miranda <gabrielmfern@outlook.com>
2024-07-07 20:26:53 -0300

    update exports for form subpath

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,7 +1,7 @@
-import { createForm, Form, useForm, acknowledgeFieldComponent } from './Form';
+import { createForm, useForm } from './Form';
 import Validators from './Validators';
 
-export { createForm, Form, useForm, Validators, acknowledgeFieldComponent };
+export { createForm, useForm, Validators };
 
 export * from './fields';
 export * from './types';
